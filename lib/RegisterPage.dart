5c6e28a8f08fb9704ba08abeff8c41a39929c2db
Chira2000 <95146759+Chira2000@users.noreply.github.com>
2023-12-23 03:20:30 +0530

    New Update Register Page

--- lib/RegisterPage.dart
+++ lib/RegisterPage.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(fit: StackFit.expand, children: [
+      Container(
+        decoration: BoxDecoration(
+            color: Colors.black,
+            image: DecorationImage(
+              image: AssetImage('assets/cof2.jpg'),
+              fit: BoxFit.cover,
+            )),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 10, 10, 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Register",
+                style: GoogleFonts.lato(
+                    fontSize: 40,
+                    color: const Color.fromARGB(255, 238, 237, 234)),
+              )
+            ],
+          ),
+        ),
+      ),
+    ]));
   }
 }
